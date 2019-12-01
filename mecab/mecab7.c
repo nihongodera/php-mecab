3e2a46594eb3a83e79695799bc040ae1695f3002
SATO Kentaro <kentaro@ranvis.com>
2019-12-01 23:45:08 +0900

    Fix use of System V dependent type.

--- mecab/mecab7.c
+++ mecab/mecab7.c
@@ -1472,7 +1472,7 @@ static PHP_FUNCTION(mecab_new)
 	if (zoptions != NULL) {
 		int getopt_result = 0;
 		zend_string *key;
-		ulong num_key;
+		zend_ulong num_key;
 		zval *entry;
 
 		ALLOC_HASHTABLE(options);
