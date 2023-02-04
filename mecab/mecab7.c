18f315101e3203c8f1b454c1477553a15504b449
SATO Kentaro <kentaro@ranvis.com>
2023-02-04 13:28:17 +0900

    Fix MeCab\version() return type in the prototype.

--- mecab/mecab7.c
+++ mecab/mecab7.c
@@ -267,7 +267,7 @@ php_mecab_check_path(const char *path, size_t length, char *real_path);
 
 /* {{{ argument informations */
 
-ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_mecab_version, ZEND_RETURN_VALUE, 0, IS_ARRAY, 0)
+ZEND_BEGIN_ARG_WITH_RETURN_TYPE_INFO_EX(arginfo_mecab_version, ZEND_RETURN_VALUE, 0, IS_STRING, 0)
 ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_mecab_split, ZEND_SEND_BY_VAL, ZEND_RETURN_VALUE, 1)
